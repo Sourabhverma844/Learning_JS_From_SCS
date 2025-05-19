5532e0dc62dc85193d8676f2a7cacee95b4d751a
Sourabh Verma <sourabhverma844@gmail.com>
2025-05-19 17:14:26 +0530

    update

---
+++ script.js
@@ -0,0 +1,26 @@
+/*
+There are many features which are frequently used in Js and they are not the part of js the language, but they are available in the browswe, and when you use them in js they are called from the browser not from js.
+
+JavaScript by itself is a programming language, but when it runs in the browser, it gets access to extra features provided by the window object.
+
+window Object Features :
+Some methods like 'alert', 'console', and 'prompt' are not built into core JS. They are part of the browser environment and come from the global 'window' object.
+------------------------------------------------------------
+| Method          | Description                            |
+|---------------- | ---------------------------------------|
+| console.log()   | Prints a normal message to the console |
+| console.warn()  | Prints a warning (yellow)              |
+| console.error() | Prints an error message (red)          |
+| alert()         | Shows a popup alert dialog             |
+| prompt()        | Shows a popup input box (text field)   |
+------------------------------------------------------------
+*/
+console.log("Sourabh Verma");
+
+console.warn("Sourabh Verma");
+
+console.error("Sourabh Verma");
+
+alert("Hey You are on Home Page");
+
+prompt("Enter Your Name : ");
